7648d224afa06a8d20db303dcb8fe08018194b7d
Joshua Davenport <87725934+jdavenport2021@users.noreply.github.com>
2023-11-01 11:19:24 -0500

    Fixing Indentation site.js

--- js/site.js
+++ js/site.js
@@ -2,24 +2,24 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-	$('#checkboxAlt').on("click", function () {
-		if ($('#interested').is(':checked')) {
-			$('#interested').prop("checked", false);
-			$('#checkboxAlt').html("<p id=\"check\"></p>");
-		}
-		else {
-			$('#interested').prop("checked", true);
-			$('#checkboxAlt').html("<p id=\"check\">&#10004;</p>");
-		}
-	});
-	$('#forInterested').on("click", function () { // For some reason for ckicking on the label to work the way it should the checked values need to be flipped?
-		if ($('#interested').is(':checked')) {
-			$('#interested').prop("checked", true);
-			$('#checkboxAlt').html("<p id=\"check\"></p>");
-	}
-		else {
-			$('#interested').prop("checked", false);
-			$('#checkboxAlt').html("<p id=\"check\">&#10004;</p>");
-		}
-	});
+  $('#checkboxAlt').on("click", function () {
+    if ($('#interested').is(':checked')) {
+      $('#interested').prop("checked", false);
+      $('#checkboxAlt').html("<p id=\"check\"></p>");
+    }
+    else {
+      $('#interested').prop("checked", true);
+      $('#checkboxAlt').html("<p id=\"check\">&#10004;</p>");
+    }
+  });
+  $('#forInterested').on("click", function () { // For some reason for ckicking on the label to work the way it should the checked values need to be flipped?
+    if ($('#interested').is(':checked')) {
+      $('#interested').prop("checked", true);
+      $('#checkboxAlt').html("<p id=\"check\"></p>");
+    }
+    else {
+      $('#interested').prop("checked", false);
+      $('#checkboxAlt').html("<p id=\"check\">&#10004;</p>");
+    }
+  });
 });
